6a3e4a2441d0db6cdb8f19c85f7e86b1b41ffef5
Christian Diaz Portela <christiandp7@gmail.com>
2021-08-20 09:57:56 -0400

    :ambulance: Fix SSR stylesheets MUI

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,4 +1,7 @@
+import React from 'react'
 import Document, { Head, Html, Main, NextScript } from 'next/document'
+import { ServerStyleSheets } from '@material-ui/core/styles'
+
 class MyDocument extends Document {
 	render() {
 		return (
@@ -10,6 +13,27 @@ class MyDocument extends Document {
 				</body>
 			</Html>
 		)
+	}
+}
+
+MyDocument.getInitialProps = async ctx => {
+	const sheets = new ServerStyleSheets()
+	const originalRenderPage = ctx.renderPage
+
+	ctx.renderPage = () =>
+		originalRenderPage({
+			enhanceApp: App => props => sheets.collect(<App {...props} />),
+		})
+
+	const initialProps = await Document.getInitialProps(ctx)
+
+	return {
+		...initialProps,
+		// Styles fragment is rendered after the app and page rendering finish.
+		styles: [
+			...React.Children.toArray(initialProps.styles),
+			sheets.getStyleElement(),
+		],
 	}
 }
 
