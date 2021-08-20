3c90d2c3b8507801ea1eaac231da5959b12bcafc
Christian Diaz Portela <christiandp7@gmail.com>
2021-08-20 19:53:27 -0400

    :lipstick: Add MainLayout

---
+++ burnna/layouts/MainLayout.tsx
@@ -0,0 +1,45 @@
+import React, { FC, useState } from 'react'
+import { createStyles, makeStyles, Theme } from '@material-ui/core/styles'
+// import Sidebar from '../components/Sidebar/Sidebar'
+// import Header from '../components/Header/Header'
+// import Footer from '../components/Footer/Footer'
+// import Cart from '../components/Cart/Cart'
+
+interface Props {
+	children: React.ReactNode
+	top: string
+	bottom: string
+}
+
+const MainLayout: FC<Props> = ({ children, ...props }) => {
+	const [sidebar, setSidebar] = useState(false)
+	const [cart, setCart] = useState(false)
+
+	const classes = useStyles(props)
+
+	return (
+		<>
+			{/* <Sidebar open={sidebar} setOpen={setSidebar} />
+			<Cart open={cart} setOpen={setCart} />
+			<Header openSidebar={setSidebar} openCart={setCart} /> */}
+			<main className={classes.main}>{children}</main>
+			{/* <Footer /> */}
+		</>
+	)
+}
+
+const useStyles = makeStyles((theme: Theme) =>
+	createStyles({
+		main: {
+			paddingTop: '65px',
+			paddingBottom: '30px',
+			minHeight: '70vh',
+			[theme.breakpoints.down('md')]: {
+				paddingTop: 0,
+				paddingBottom: '12px',
+			},
+		},
+	}),
+)
+
+export default MainLayout
