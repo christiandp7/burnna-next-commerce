25343d86ffa8cb1b5482de3b5b1b457c9492ab9c
Christian Diaz Portela <christiandp7@gmail.com>
2021-08-21 09:23:03 -0400

    :building_construction: AddFAQ Layout Successfully 60%

---
+++ burnna/layouts/FAQLayout.tsx
@@ -0,0 +1,71 @@
+import React, { FC, useState } from 'react'
+import { createTheme, makeStyles, Theme } from '@material-ui/core/styles'
+// import FAQSidebar from '../components/FAQSidebar/FAQSidebar'
+// import Cart from '../components/Cart/Cart'
+// import { Scrollbars } from 'react-custom-scrollbars-2'
+import { Header, Footer } from '@burnna/components'
+
+const drawerWidth = 190
+
+interface Props {
+	top?: string
+	bottom?: string
+	mobTop?: string
+	alignItemsCenter?: boolean
+}
+
+const InfoLayout: FC<Props> = ({ children, ...props }) => {
+	const [sidebar, setSidebar] = useState(false)
+	const [cart, setCart] = useState(false)
+
+	const classes = useStyles(props)
+
+	return (
+		<div className={classes.root}>
+			{/* <FAQSidebar open={sidebar} setOpen={setSidebar} /> */}
+			{/* <Cart open={cart} setOpen={setCart} /> */}
+			{/* <Scrollbars
+				style={{ height: '100vh' }}
+				className={classes.scrollbars}
+				autoHide
+				autoHideTimeout={2000}
+				autoHideDuration={300}> */}
+			<Header
+				infoLayout
+				// openSidebar={setSidebar}
+				// openCart={setCart}
+				// menu={false}
+			/>
+			<main className={classes.main}>{children}</main>
+			<Footer />
+			{/* </Scrollbars> */}
+		</div>
+	)
+}
+
+const useStyles = makeStyles((theme: Theme) =>
+	createTheme({
+		root: {
+			display: 'flex',
+		},
+		main: props => ({
+			paddingTop: props.top || '65px',
+			paddingBottom: props.bottom || '30px',
+			minHeight: 'calc(100vh - 65px)',
+			display: props.alignItemsCenter ? 'flex' : 'block',
+			alignItems: 'center',
+			[theme.breakpoints.down('md')]: props => ({
+				paddingTop: props.mobTop || '40px',
+				paddingBottom: props.bottom || '12px',
+			}),
+		}),
+		scrollbars: {
+			[theme.breakpoints.up('md')]: {
+				marginLeft: '190px',
+			},
+			// flexGrow: 1,
+		},
+	}),
+)
+
+export default InfoLayout
--- burnna/layouts/index.ts
+++ burnna/layouts/index.ts
@@ -1,2 +1,3 @@
 export { default as HomeLayout } from './HomeLayout'
 export { default as MainLayout } from './MainLayout'
+export { default as FAQLayout } from './FAQLayout'
