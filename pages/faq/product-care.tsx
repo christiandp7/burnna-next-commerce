9c3106e49486ce0d8c649d6380c55f3f993054bb
Christian Diaz Portela <christiandp7@gmail.com>
2021-08-21 21:48:21 -0400

    :lipstick: Add Size Guide and Product Care

---
+++ pages/faq/product-care.tsx
@@ -0,0 +1,8 @@
+import React, { FC } from 'react'
+import { FAQLayout } from '@burnna/layouts'
+
+const ProductCare: FC = () => {
+	return <FAQLayout>Product Care</FAQLayout>
+}
+
+export default ProductCare
---
+++ pages/faq/size-guide.tsx
@@ -0,0 +1,8 @@
+import React, { FC } from 'react'
+import { FAQLayout } from '@burnna/layouts'
+
+const SizeGuide: FC = () => {
+	return <FAQLayout>Size Guide</FAQLayout>
+}
+
+export default SizeGuide
