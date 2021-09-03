c3aeb4345503a808356698e37a935122062d2d78
Christian Diaz Portela <christiandp7@gmail.com>
2021-09-02 23:36:21 -0400

    :ambulance: Fix campaign page

--- pages/campaign.tsx
+++ pages/campaign.tsx
@@ -3,14 +3,14 @@ import Image from 'next/image'
 import { makeStyles, createStyles, Theme } from '@material-ui/core'
 
 const campaign = () => {
-	const classes = useStyles()
+	// const classes = useStyles()
 	return <div>campaign</div>
 }
 
-const useStyles = makeStyles((theme: Theme) =>
-	createStyles({
-		root: {},
-	}),
-)
+// const useStyles = makeStyles((theme: Theme) =>
+// 	createStyles({
+// 		root: {},
+// 	}),
+// )
 
 export default campaign
