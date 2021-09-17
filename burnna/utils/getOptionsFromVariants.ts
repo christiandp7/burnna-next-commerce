4ac9b7131f6d8c84ca64cb88733aa7189f158efc
Christian Diaz Portela <christiandp7@gmail.com>
2021-09-17 00:23:12 -0400

    :sparkles: getOptionsFromVariantsReady to draw

--- burnna/utils/getOptionsFromVariants.ts
+++ burnna/utils/getOptionsFromVariants.ts
@@ -1,13 +1,19 @@
 import { ProductVariant } from '@commerce/types/product'
 import { ProductOptionValues } from '@commerce/types/product'
 
+export type ColorOptions = {
+	label: string
+	hexColors?: string[] | undefined
+	variantId: string | number
+}
+
 export function getOptionsFromVariants(variants: ProductVariant[]) {
-	let data: ProductOptionValues[] = []
+	let data: ColorOptions[] = []
 	variants.forEach(variant => {
 		variant.options.forEach(opt => {
 			if (opt.displayName.match(/colou?r/gi)) {
 				if (!data.find(item => item.label === opt.values[0].label)) {
-					data.push({ ...opt.values[0] })
+					data.push({ ...opt.values[0], variantId: variant.id })
 				}
 			}
 		})
