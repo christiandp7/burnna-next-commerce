7b3d13f0b49dd07eeeb4001e1e4e975043e0d0c0
Christian Diaz Portela <christiandp7@gmail.com>
2021-09-20 19:17:01 -0400

    :alien: Fetch about content from shopify api successfully

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,28 +1,43 @@
 import React, { FC } from 'react'
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
-import type { GetStaticPropsContext } from 'next'
+import type { GetStaticPropsContext, InferGetStaticPropsType } from 'next'
 import { createStyles, makeStyles, Theme } from '@material-ui/core/styles'
 import Grid from '@material-ui/core/Grid'
 import Typography from '@material-ui/core/Typography'
-// import { LazyLoadImage } from 'react-lazy-load-image-component'
+import parse from 'html-react-parser'
 import Image from 'next/image'
 //  components
 import { MainLayout } from '@burnna/layouts'
 import { Layout } from '@components/common'
-// import Heading from '../components/Typography/Heading'
-// assets
-// import { HugeTitleAbout } from '@burnna/svg'
+// lib
+import commerce from '@lib/api/commerce'
 
-export async function getStaticProps({ locale }: GetStaticPropsContext) {
+export async function getStaticProps({
+	locale,
+	locales,
+	preview,
+}: GetStaticPropsContext) {
+	const config = { locale, locales }
+	const pagePromise = commerce.getPage({
+		variables: { id: 'Z2lkOi8vc2hvcGlmeS9QYWdlLzg1MTUxNTgwMzQ1' },
+		config,
+		preview,
+	})
+	const { page } = await pagePromise
 	const i18n = await serverSideTranslations(locale!, ['common'])
+	console.log(page)
 	return {
 		props: {
+			page,
 			...i18n,
 		},
 	}
 }
 
-function About() {
+// function About() {
+function About({ page }: InferGetStaticPropsType<typeof getStaticProps>) {
+	// console.log(page)
+	const textContent = page ? page?.body : ''
 	const classes = useStyles()
 	return (
 		<MainLayout>
@@ -43,50 +58,7 @@ function About() {
 						</div>
 					</Grid>
 					<Grid item xs={12} md={6}>
-						<div className={classes.rte}>
-							<Typography className={classes.title} component="h2" variant="h6">
-								Who are we?
-							</Typography>
-							<Typography component="p" variant="body1">
-								A brand propelled by the firm belief that great things come from
-								connection. Connection to one’s self and between extraordinary
-								individuals who have a life’s vision and act upon it.
-							</Typography>
-							<Typography component="p" variant="body1">
-								BURNNA was created, not with the sole purpose of selling beach and
-								resort-wear, but rather more with the intention of advocating for a
-								lifestyle propelled by the leisure and exoticism that fill our native
-								lands through digital narratives and actionable content.
-							</Typography>
-						</div>
-						<div className={classes.rte}>
-							<Typography className={classes.title} component="h2" variant="h6">
-								How were we created?
-							</Typography>
-							<Typography component="p" variant="body1">
-								BURNNA was founded in 2021 by Antonella Camargo and Laura Harada.
-								Both stem their roots in different regions of Latin America, one with
-								an Andean spirit and the other with a Caribbean soul, this
-								complementation became an essential aspect of the decision-making
-								process when offering a multifaceted brand experience.
-							</Typography>
-							<Typography component="p" variant="body1">
-								Upon meeting in university they shared a passion for traveling,
-								connecting with new people and experiencing life from outside their
-								comfort zone. This brewed a depth of appreciating different
-								backgrounds and recognising the beauty in the contrasts of each.
-								“Inspired by the ideas and experiences collected over the years, we
-								decided to combine our love of fashion, art, photography, culture,
-								and joie de vivre to create a platform that expresses our beliefs.”
-							</Typography>
-							<Typography component="p" variant="body1">
-								Despite the newborn challenges sparked during and post the pandemic,
-								they ceased to see an opportunity with buoyancy and resilience in
-								every step of the way, approaching the whole process of starting a
-								brand with much more perspective and resourcefulness than it would’ve
-								been required in different times.
-							</Typography>
-						</div>
+						<div className={classes.rte}>{parse(textContent)}</div>
 					</Grid>
 				</Grid>
 			</div>
@@ -97,7 +69,6 @@ function About() {
 const useStyles = makeStyles((theme: Theme) =>
 	createStyles({
 		pageWrapper: {
-			// marginTop: theme.spacing(4),
 			marginBottom: theme.spacing(2),
 			[theme.breakpoints.down('md')]: {
 				marginTop: 0,
@@ -114,6 +85,16 @@ const useStyles = makeStyles((theme: Theme) =>
 		rte: {
 			marginTop: theme.spacing(8),
 			padding: `${theme.spacing(1)}px ${theme.spacing(3)}px`,
+			'& h2': {
+				textTransform: 'uppercase',
+				fontWeight: 700,
+				fontSize: theme.typography.h5.fontSize,
+				textUnderlineOffset: '4px',
+				marginBottom: theme.spacing(2),
+				[theme.breakpoints.down('sm')]: {
+					marginBottom: theme.spacing(1),
+				},
+			},
 			'& p': {
 				marginBottom: '16px',
 			},
@@ -130,16 +111,6 @@ const useStyles = makeStyles((theme: Theme) =>
 			marginTop: theme.spacing(3),
 			[theme.breakpoints.down('sm')]: {
 				fontSize: theme.spacing(6),
-				marginBottom: theme.spacing(2),
-			},
-		},
-		title: {
-			textTransform: 'uppercase',
-			fontWeight: 700,
-			fontSize: theme.typography.h5.fontSize,
-			textUnderlineOffset: '4px',
-			marginBottom: theme.spacing(3),
-			[theme.breakpoints.down('sm')]: {
 				marginBottom: theme.spacing(2),
 			},
 		},
